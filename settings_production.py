c3d0136f6f56b169aa1051dcab2cb16f5e15da91
sqs <sqs@stanford.edu>
2010-02-12 17:16:21 +0000

    different template dir for production

--- settings_production.py
+++ settings_production.py
@@ -1,8 +1,9 @@
 from openelections.settings import *
-
 DATABASE_ENGINE = 'mysql'
 DATABASE_NAME = 'oe_production'
 DATABASE_USER = 'oe_production'
-DATABASE_PASSWORD = ''
+DATABASE_PASSWORD = 'so36v5ri6yaoncgzi3m8vdsg23yria5tkpbghaqs'
 DATABASE_HOST = ''
 DATABASE_PORT = ''
+
+TEMPLATE_DIRS = ('/var/www/openelections/templates/',)
