264b294ccef856a98a8dabd19361f44c27e8fb2b
sqs <sqs@stanford.edu>
2010-02-12 09:17:59 -0800

    hash authentication in webauth

--- auth/stanford_webauth.py
+++ auth/stanford_webauth.py
@@ -1,49 +1,45 @@
-from django.http import HttpResponseRedirect 
-from django.http import HttpResponseServerError
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError
 from lxml import etree
 import urllib2
+import hashlib
 
 AUTH_URL = "https://weblogin.stanford.edu" 
 CAS_NS = {'cas': 'http://www.yale.edu/tp/cas'}
+WEBAUTH_SECRET = '2aca147905asioerhdf67fds7y7ydsyhdsg6afbfbyfbbo4957'
 
-def webauth_required2(function):
-  def _dec(view_func):
-    def _view(request, *args, **kwargs):
-      if request.session.get('webauth', False): 
-        return view_func(request, *args, **kwargs)
-      ticket = request.GET.get("ticket", False)
-      if ticket:
-        try:
-          url = request.session.get('url', '')
-          handle = urllib2.urlopen(AUTH_URL + "/serviceValidate?service=" + url + "&ticket=" + ticket)
-        except IOError, e:
-          return HttpResponseServerError()
-	tree = etree.parse(handle)
-	request.session['webauth'] = (len(tree.xpath('//cas:authenticationSuccess', namespaces=CAS_NS)) == 1)
-	if request.session['webauth']:
-	  request.session['netid'] = tree.xpath('//cas:user', namespaces=CAS_NS)[0].text
-          return view_func(request, *args, **kwargs)
-      else:
-        request.session['url'] = urllib2.quote(request.build_absolute_uri())
-        return HttpResponseRedirect(AUTH_URL + "/login?service=" + request.session['url'])
-    _view.__name__ = view_func.__name__
-    _view.__dict__ = view_func.__dict__
-    _view.__doc__ = view_func.__doc__
-    
-    return _view
-  return _dec(function)
+def make_secret_hash(sunetid):
+    m = hashlib.md5()
+    m.update(WEBAUTH_SECRET + sunetid)
+    return m.hexdigest()
 
 WEBAUTH_URL = "http://stanford.edu/~sqs/cgi-bin/authenticate_elections2.php?from="
 
 def webauth_required(function):
     def _dec(view_func):
         def _view(request, *args, **kwargs):
-            #request.session['webauth_sunetid'] = 'sqs'
+            # if 'webauth_sunetid' in request.session:
+            #    del request.session['webauth_sunetid']
+            #    del request.session['webauth_name']
             if 'webauth_sunetid' in request.session:
                 return view_func(request, *args, **kwargs)
             elif 'webauth_sunetid' in request.GET:
-                request.session['webauth_sunetid'] = request.GET['webauth_sunetid']
-                return view_func(request, *args, **kwargs)
+                sunetid = request.GET['webauth_sunetid']
+                actual_hash = request.GET['hash']
+                #name = request.GET['name']
+                
+                expected_hash = make_secret_hash(sunetid)
+                
+                if expected_hash == actual_hash:
+                    request.session['webauth_sunetid'] = sunetid
+                    #request.session['webauth_name'] = name
+                    
+                    path_only = request.path
+                    upto = request.path.find('?')
+                    if upto != -1:
+                        path_only = path_only[:upto]
+                    return HttpResponseRedirect(path_only)
+                else:
+                    return HttpResponse("authentication error")
             else:
                 url = urllib2.quote(request.build_absolute_uri())
                 return HttpResponseRedirect(WEBAUTH_URL + url)
