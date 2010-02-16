9bc4c57cfe9947ac231fb7762a7f064e8b38b409
sqs <sqs@stanford.edu>
2010-02-16 03:40:12 -0800

    issue manage require webauth tests

--- issues/tests.py
+++ issues/tests.py
@@ -3,9 +3,14 @@ from openelections.issues.models import Electorate, Issue
 from openelections.petitions.models import Signature
 
 
-class VisitorTest(TestCase):
-    fixtures = ['2009ballot.json']
+class UnauthenticatedVisitorTest(OETestCase):    
+    def test_manage_index_requires_webauth(self):
+        self.assertPathRequiresWebAuth('/issues/manage/')
+        
+    def test_manage_new_requires_webauth(self):
+        self.assertPathRequiresWebAuth('/issues/manage/new/US')
+
+    def test_manage_create_requires_webauth(self):
+        res = self.client.post('/issues/manage/create')
+        self.assertResponseRequiresWebAuth(res)
     
-    def test_index(self):
-        res = self.client.get('/issues/zack-warma')
-        self.failUnlessEqual(res.status_code, 200)
