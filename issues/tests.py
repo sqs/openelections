b99ee32108269834ac3063695321dd623982f5a1
sqs <sqs@stanford.edu>
2010-02-16 02:00:35 -0800

    issues/detail basic test

--- issues/tests.py
+++ issues/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
 class VisitorTest(TestCase):
+    fixtures = ['2009ballot.json']
+    
     def test_index(self):
-        res = self.client.get('/issues/1')
+        res = self.client.get('/issues/zack-warma')
         self.failUnlessEqual(res.status_code, 200)
