80843f9f126cc9f5f6995b8952f89526ec89a58b
sqs <sqs@stanford.edu>
2010-02-22 16:35:04 -0800

    settings and paths for staging

---
+++ settings_staging.py
@@ -0,0 +1,9 @@
+from openelections.settings import *
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'oe_staging'
+DATABASE_USER = 'oe_staging'
+DATABASE_PASSWORD = 'sdfjef77fdsh76g3a7hauak46drbgkinajghe'
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+TEMPLATE_DIRS = ('/var/www/staging/openelections/templates/',)
