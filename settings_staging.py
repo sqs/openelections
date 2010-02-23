ca5be9fa01f7e431c0a1ca379f5ecb9522691fa0
sqs <sqs@stanford.edu>
2010-02-23 03:17:46 +0000

    staging settings

--- settings_staging.py
+++ settings_staging.py
@@ -1,9 +1,12 @@
 from openelections.settings import *
+DEBUG = TEMPLATE_DEBUG = True
 DATABASE_ENGINE = 'mysql'
 DATABASE_NAME = 'oe_staging'
 DATABASE_USER = 'oe_staging'
 DATABASE_PASSWORD = 'sdfjef77fdsh76g3a7hauak46drbgkinajghe'
 DATABASE_HOST = ''
 DATABASE_PORT = ''
+
+MEDIA_ROOT = '/var/www/staging/openelections/media/'
 
 TEMPLATE_DIRS = ('/var/www/staging/openelections/templates/',)
